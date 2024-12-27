def0596a83ac462beba23a0da49e1250e40378d7
Fadli <mf273404@gmail.com>
2024-12-27 21:30:55 +0700

    Fadli ngepush

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,78 +1,54 @@
-
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.87.0">
-    <title>Signin Template · Bootstrap v5.1</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/sign-in/">
-
-
-
-    <!-- Bootstrap core CSS -->
-    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
-
+    <title>Welcome!!!</title>
     <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
+      html, body {
+        height: 100%;
+        margin: 0;
       }
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
+      body {
+        background-image: url('https://christinapotvin.com/wp-content/uploads/2022/10/Nasi-Goreng.jpg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .form-signin {
+        background-color: white;
+        opacity: 0.8; 
+        padding: 20px;
+        border-radius: 10px; 
+        width: 350px; 
+      }
+      .btn-container {
+        display: flex;
+        justify-content: space-between;
       }
     </style>
-
-
-    <!-- Custom styles for this template -->
-    <link href="{{ asset('/css/signin.css') }}" rel="stylesheet">
   </head>
   <body class="text-center">
-
-<main class="form-signin">
-  <form action="{{ route('auth.authenticate') }}" method="POST">
-      @csrf
-    <img class="mb-4" src="{{ asset('img/bootstrap-logo.svg') }}" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-    <div class="form-floating">
-      <input type="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" id="email" name="email" placeholder="name@example.com">
-      <label for="email">Email address</label>
-      @error('email')
-      <div class="invalid-feedback">
-      {{ $message }}
+    <main>
+      <div class="form-signin">
+        <h2>Selamat Datang di Kedai Silvi </h2>
+        <form action="">
+          <label for="username">Username</label>
+          <br>
+          <input style="width: 85%;" type="text" id="username" placeholder="Silahkan Masukkan Username!!!" required>
+          <br>
+          <br>
+          <label for="password">Password</label>
+          <br>
+          <input style="width: 85%;" type="password" id="password" placeholder="Silahkan Masukkan Password!!!" required>
+          <br>
+          <p style="margin-top:2px;">belum punya akun? <a href="">silahkan register disini</a></p>
+          <button type="submit" class="btn btn-primary">Masuk</button>
+        </form>
       </div>
-      @enderror
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password" placeholder="Password">
-      <label for="password">Password</label>
-      @error('password')
-      <div class="invalid-feedback">
-      {{ $message }}
-      </div>
-      @enderror
-    </div>
-
-    <div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
-    </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-  </form>
-</main>
-
-
-
+    </main>
   </body>
 </html>
